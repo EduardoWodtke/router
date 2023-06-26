25f076ae4f7ef19538060084323eb321d64a5698
EduardoWodtke <eduardorwodtkeifc@gmail.com>
2023-06-26 11:17:05 -0300

    router

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
-<script setup></script>
+<script setup>
+  // import AboutView from "./views/AboutView.vue";
+  // import HomeView from "./views/HomeView.vue";
+</script>
 
 <template>
   <header>HEADER</header>
-  <aside>MENU</aside>
-  <main>MAIN</main>
+  <aside>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <br/>
+      <br/>
+      <router-link to="/about">About</router-link>
+    </nav>
+  </aside>
+  <main>
+    <router-view/>
+  </main>
   <footer>FOOTER</footer>
 </template>
 
